d9403c722bedc208707daeb60c60f0eec56a6a2c
Ivan <ipkozyrin@gmail.com>
2025-10-10 13:32:28 -0300

    add

--- micro-file-cache/src/app.module.ts
+++ micro-file-cache/src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { getConfig } from './config/app.config';
+import { HealthController } from './common/controllers/health.controller';
 
 @Module({
   imports: [
+    // Глобальная конфигурация
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      load: [getConfig],
     }),
+
+    // Модуль для cron jobs (автоматическая очистка)
+    ScheduleModule.forRoot(),
   ],
-  controllers: [],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
